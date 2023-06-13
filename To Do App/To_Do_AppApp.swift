7780d7d9e2ba39fe8ca483530d7ffdff67b3af2f
Kylie Nguyen <nguyenm.kylie@gmail.com>
2023-06-13 08:18:06 -0700

    Initial Commit

---
+++ To Do App/To_Do_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_AppApp.swift
+//  To Do App
+//
+//  Created by Kylie Nguyen on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
